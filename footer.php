208b388524f949b2967a85ded5a653b08488d1a0
Xenderrr <90962222+Xenderrr@users.noreply.github.com>
2023-12-31 01:01:28 +0300

    Blocks that are repeated on every page

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="foot">
+        <p>
+            <a name="contacts"></a>
+        </p>
+        <div class="c">
+            <h3>Location</h3>
+            <p>Leningradsky Ave, 26/1, Moscow, 125040</p>
+        </div>
+        <div class="c">
+            <h3>Fly Away in social media</h3>
+            <div class="soc">
+                <a href="https://t.me/Xxxenderrr" class="link"><img src="images/links/tg.png" class="link"></a>
+                <a href="https://instagram.com/ksusha_tak_givet?igshid=ZDdkNTZiNTM=" class="link"><img src="images/links/ig.png" class="link"></a>
+                <a href="https://mail.google.com/mail/u/0/#inbox" class="link"><img src="images/links/google.png" class="link"></a>
+                <a href="https://vk.com/ksushaslife" class="l1"><img src="images/links/vk.png" class="l1"></a>
+                <a href="https://www.youtube.com/channel/UCFKqRSsJ0r8OCP2v3XVNXtQ" class="l1"><img src="images/links/youtube.png" class="l1"></a>
+            </div>
+        </div>
+        <div class="c">
+            <h3>Contact number</h3>
+            <p>8-906-294-65-81</p>
+        </div>
+    </div>
+    <div class="copy">
+        Copyryght<br>&copy;Xender <?php echo date('Y')?>
+    </div>
+</footer>
